4d0a5e4ab3ea2fca2840c2ea048875ff9e457361
Cory Boudreaux <cboud31@gmail.com>
2024-03-04 20:33:56 -0600

    finish layout, button styling & padding, toggle event listener

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Connected!");
+const TOGGLE_BUTTON_CLASS = "toggle";
+
+const toggleButtons = [];
+const colorBtn = document.querySelector(".btn-color");
+const rainbowBtn = document.querySelector(".btn-rainbow");
+toggleButtons.push(colorBtn, rainbowBtn);
+
+setToggleButton = (button) => {
+  return button.addEventListener("click", () => {
+    if (button.classList.contains(TOGGLE_BUTTON_CLASS)) {
+      button.classList.remove(TOGGLE_BUTTON_CLASS);
+    } else {
+      button.classList.add(TOGGLE_BUTTON_CLASS);
+    }
+  });
+};
+
+toggleButtons.forEach((button) => {
+  setToggleButton(button);
+});
